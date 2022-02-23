02d5af2d7a37a3ac53ec69731391df05fb9ab7a7
Rajkawale7 <Rajkawale712@gmail.com>
2022-02-23 21:10:38 +0530

    Added: Flipping coin

---
+++ flipCoin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Extend the Flip Coin problem till either Heads or Tails wins 11 times.
+
+head=0
+tail=0
+
+while (( $head != 11 && $tail != 11 ))
+do
+   if (( $RANDOM%2 = 0 ))
+   then
+        (( head++ ))
+   else
+        (( tail++ ))
+   fi
+done
+
+echo "Head count: " $head
+echo "Tail count: " $tail
+
